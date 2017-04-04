adfd1131b8fe1484e161f2d22cdf355a88355e11
Simona Colapicchioni <s.colapicchioni@gmail.com>
2017-04-04 09:57:45 +0200

    Step01 Solution

---
+++ MarketPlace/MarketPlaceService/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MarketPlaceService.Models;
+using MarketPlaceService.Data;
+
+namespace MarketPlaceService.Controllers {
+    [Route("api/[controller]")]
+    public class ProductsController : Controller {
+        private readonly IProductsRepository _ProductsRepository;
+
+        public ProductsController(IProductsRepository ProductsRepository) {
+            _ProductsRepository = ProductsRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetAll() {
+            return _ProductsRepository.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetProduct")]
+        public IActionResult GetById(int id) {
+            var item = _ProductsRepository.Find(id);
+            if (item == null) {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Product product) {
+            if (product == null) {
+                return BadRequest();
+            }
+
+            _ProductsRepository.Add(product);
+
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Product product) {
+            if (product == null || product.Id != id) {
+                return BadRequest();
+            }
+
+            var original = _ProductsRepository.Find(id);
+            if (original == null) {
+                return NotFound();
+            }
+
+            original.Name = product.Name;
+            original.Description = product.Description;
+            original.Price = product.Price;
+
+            _ProductsRepository.Update(original);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) {
+            var product = _ProductsRepository.Find(id);
+            if (product == null) {
+                return NotFound();
+            }
+
+            _ProductsRepository.Remove(id);
+            return new NoContentResult();
+        }
+    }
+}
---
+++ MarketPlace/MarketPlaceService/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MarketPlaceService.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketPlaceService
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // requires using Microsoft.EntityFrameworkCore;
+            // and using MarketPlaceService.Data;
+            services.AddDbContext<MarketPlaceContext>(opt => opt.UseInMemoryDatabase());
+
+            // Add framework services.
+            services.AddMvc();
+
+            // requires using MarketPlaceService.Models;
+            services.AddSingleton<IProductsRepository, ProductsRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
